e05cb17f0f7e8abd65383ada321ef936443bfc98
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-13 22:52:04 +0000

    feat :Creating login form

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -26,6 +26,7 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatSort, MatSortHeader} from "@angular/material/sort";
 import {MatFormField, MatInput} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -58,6 +59,7 @@ import {MatFormFieldModule} from "@angular/material/form-field";
     MatSortHeader,
     MatInput,
     MatFormFieldModule,
+    ReactiveFormsModule,
   ],
   providers: [
     provideAnimationsAsync()
