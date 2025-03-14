15c8bd714a87586dcbf2500219ef1512ed212148
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-14 23:34:43 +0000

    feat :added logout

--- frontend-angular/src/app/services/authentication.service.ts
+++ frontend-angular/src/app/services/authentication.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  public username!:string;
-  public password!:string;
+  public username:any;
   public roles:any;
+  public authenticated:boolean=false;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   public login(username:string,password:string){
     if(username=="admin" && password=="1234"){
       this.username=username;
       this.roles=['ADMIN'];
+      this.authenticated=true;
       return true;
     }else{
       return false;
     }
+  }
+  logout(){
+    this.authenticated=false;
+    this.username=undefined;
+    this.roles=undefined;
+    this.router.navigateByUrl("/login")
   }
 }
