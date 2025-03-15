74e90b550d87c0bf210a4ae76d3fc4ffe2863903
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-15 14:13:01 +0000

    feat :Create backend project

---
+++ backend-students-app/src/main/java/ma/abdellah/backendstudentsapp/BackendStudentsAppApplication.java
@@ -0,0 +1,13 @@
+package ma.abdellah.backendstudentsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStudentsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStudentsAppApplication.class, args);
+	}
+
+}
