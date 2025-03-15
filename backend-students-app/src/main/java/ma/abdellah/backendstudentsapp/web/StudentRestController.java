858b3f8189204d30fbb734cc8f61b0500cc31c1f
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-15 22:59:45 +0000

    feat : add StudentRestController

---
+++ backend-students-app/src/main/java/ma/abdellah/backendstudentsapp/web/StudentRestController.java
@@ -0,0 +1,47 @@
+package ma.abdellah.backendstudentsapp.web;
+
+import ma.abdellah.backendstudentsapp.entities.Payment;
+import ma.abdellah.backendstudentsapp.entities.Student;
+import ma.abdellah.backendstudentsapp.repository.PaymentRepository;
+import ma.abdellah.backendstudentsapp.repository.StudentRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentRestController {
+    private PaymentRepository paymentRepository;
+    private StudentRepository studentRepository;
+
+    public StudentRestController(PaymentRepository paymentRepository, StudentRepository studentRepository) {
+        this.paymentRepository = paymentRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/payments")
+    public List<Payment> allPayments() {
+        return paymentRepository.findAll();
+    }
+
+    @GetMapping("/payments/{id}")
+    public Payment findById(@PathVariable Long id){
+        return paymentRepository.findById(id).get();
+    }
+
+    @GetMapping("/students")
+    public List<Student> allStudents(){
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/students/{id}")
+    public Student findById(@PathVariable String Id){
+        return studentRepository.findById(Id).get();
+    }
+
+    @GetMapping("/students/{code}/payments")
+    public List<Payment> findByStudentCode(@PathVariable String code){
+        return paymentRepository.findByStudentCode(code);
+    }
+}
