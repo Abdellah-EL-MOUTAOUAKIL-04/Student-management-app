d4c18909b87597ab05a02309cd6a2cc762f2df9f
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-15 14:34:49 +0000

    feat :add Student & Payment entities

---
+++ backend-students-app/src/main/java/ma/abdellah/backendstudentsapp/entities/PaymentStatus.java
@@ -0,0 +1,5 @@
+package ma.abdellah.backendstudentsapp.entities;
+
+public enum PaymentStatus {
+    CREATED,VALIDATED,REJECTED
+}
---
+++ backend-students-app/src/main/java/ma/abdellah/backendstudentsapp/entities/PaymentType.java
@@ -0,0 +1,5 @@
+package ma.abdellah.backendstudentsapp.entities;
+
+public enum PaymentType {
+    CASH,CHECK,TRANSFER,DEPOSIT
+}
