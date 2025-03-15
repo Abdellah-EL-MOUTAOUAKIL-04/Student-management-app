c196f2256b8a79858674aba55989397ab0b98465
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-15 22:51:44 +0000

    feat : add h2 db to app proprities and add a bean to generate some random data

--- backend-students-app/src/main/java/ma/abdellah/backendstudentsapp/BackendStudentsAppApplication.java
+++ backend-students-app/src/main/java/ma/abdellah/backendstudentsapp/BackendStudentsAppApplication.java
@@ -1,7 +1,18 @@
 package ma.abdellah.backendstudentsapp;
 
+import ma.abdellah.backendstudentsapp.entities.Payment;
+import ma.abdellah.backendstudentsapp.entities.PaymentType;
+import ma.abdellah.backendstudentsapp.entities.Student;
+import ma.abdellah.backendstudentsapp.repository.PaymentRepository;
+import ma.abdellah.backendstudentsapp.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.UUID;
 
 @SpringBootApplication
 public class BackendStudentsAppApplication {
@@ -10,4 +21,29 @@ public class BackendStudentsAppApplication {
 		SpringApplication.run(BackendStudentsAppApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository, PaymentRepository paymentRepository) {
+		return args -> {
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).code("112233").firstName("Abdellah").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).code("112244").firstName("Ahmed").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).code("112255").firstName("Zahra").build());
+
+			PaymentType[] paymentTypes = PaymentType.values();
+			Random random=new Random();
+
+			studentRepository.findAll().forEach(st->{
+				int index=random.nextInt(paymentTypes.length);
+				for (int i = 0; i < 10; i++) {
+					Payment payment=Payment.builder()
+							.amount(1000+(int)(Math.random()*1000))
+							.date(LocalDate.now())
+							.type(paymentTypes[index])
+							.file(UUID.randomUUID().toString())
+							.student(st)
+							.build();
+					paymentRepository.save(payment);
+				}
+			});
+		};
+	}
 }
