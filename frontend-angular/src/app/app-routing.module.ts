701f71cb853bfc8f8a1899836a7b9e345c5fafd5
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-26 23:17:49 +0000

    Fix : added student-details

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import {LoadStudentsComponent} from "./load-students/load-students.component";
 import {AdminTemplateComponent} from "./admin-template/admin-template.component";
 import {AuthGuard} from "./guards/auth.guard";
 import {AuthorizationGuard} from "./guards/authorization.guard";
+import {StudentDetailsComponent} from "./student-details/student-details.component";
 
 const routes: Routes = [
   {path:"",component:LoginComponent},
@@ -21,6 +22,7 @@ const routes: Routes = [
       {path:"dashboard",component:DashboardComponent},
       {path:"students",component:StudentsComponent},
       {path:"payments",component:PaymentsComponent},
+      {path:"student-details/:code",component:StudentDetailsComponent},
       {path:"loadPayments",component:LoadPaymentsComponent,canActivate:[AuthorizationGuard],data:{roles:['ADMIN']}},
       {path:"loadStudents",component:LoadStudentsComponent,canActivate:[AuthorizationGuard],data:{roles:['ADMIN']}},
     ]},
---
+++ frontend-angular/src/app/student-details/student-details.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {StudentsService} from "../services/students.service";
+import {Payment} from "../model/students.model";
+import {MatTableDataSource} from "@angular/material/table";
+
+@Component({
+  selector: 'app-student-details',
+  templateUrl: './student-details.component.html',
+  styleUrl: './student-details.component.css'
+})
+export class StudentDetailsComponent implements OnInit {
+  studentCode!:string;
+  studentPayments!:Array<Payment>;
+  paymentDataSource!:MatTableDataSource<Payment>;
+  public displayedColumns=['id','date','amount','type','status','firstName'];
+
+
+  constructor(private route:ActivatedRoute,private studentService:StudentsService) {
+  }
+
+  ngOnInit(): void {
+    this.studentCode=this.route.snapshot.params['code'];
+    this.studentService.getStudentsPayments(this.studentCode).subscribe({
+        next:data => {
+          this.studentPayments=data;
+          this.paymentDataSource=new MatTableDataSource<Payment>(this.studentPayments);
+          },
+        error:err=> {
+          console.log(err);
+        }
+      });
+  }
+
+}
